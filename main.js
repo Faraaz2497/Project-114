26a4b26ff65c5b3a3f591ddaceeda5e62776f40d
Faraaz2497 <76690389+Faraaz2497@users.noreply.github.com>
2022-05-06 17:08:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-function preload(){
+noseX=0;
+noseY=0;
+
+function preload() {
+    img=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
-function setup(){
-    canvas=createCanvas(300,300);
-    canvas.center();
-    video=createCapture(VIDEO);
-    video.hide();
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',getPoses)
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(200,170);
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
-function draw(){
-    image(video,0,0,300,300);
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
-function takeSnapshot(){
-    save('Mustache/Lipstick Image.png');
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+  }
 }
-function getPoses(result){
-    if (result.length>0){
-        console.log(result);
-        console.log("nose x="+result[0].pose.nose.x);
-        console.log("nose y="+result[0].pose.nose.y);
-    }
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+ image(img,noseX-40,noseY+5,75,30);
 }
-function modelLoaded(){
-    console.log("Model is Loaded");
+
+function take_snapshot(){    
+  save('MustacheImage.png');
 }
